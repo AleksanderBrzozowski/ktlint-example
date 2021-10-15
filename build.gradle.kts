0e101087140daebbaab8b4cc51fffc0f9fa44896
Aleksander Brzozowski <aleksander.brzozowski@allegro.pl>
2021-10-15 12:00:39 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+}
+
+group = "pl.brzozowski"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+configure<KtlintExtension> {
+    version.set("0.43.0-SNAPSHOT")
+    debug.set(true)
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,4 @@
+fun main() {
+    // there is an indentation error :) - Unexpected indentation (0) (should be 4)
+println("Hello, World!")
+}
